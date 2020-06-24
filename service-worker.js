6ad5027d3fb46dd3fb26c939bf0f199705265ba2
NiceDotExe <64508137+NiceDotExe@users.noreply.github.com>
2020-06-23 21:54:45 -0500

    v 3.0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.1a2e6bd34df5212fbb3851faa58e2898.js"
+  "precache-manifest.ce24fb1c888b662bdb84a9d63845681f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "reciclaje"});
